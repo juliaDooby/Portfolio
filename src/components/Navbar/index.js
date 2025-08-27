7484e10c3e5972b243fe5494b521c9f2768ec8fb
Julia: @juliaDooby <juliadooby@yandex.ru>
2025-08-27 21:43:21 +0300

    Add files via upload

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -27,7 +27,7 @@ const Navbar = () => {
           <NavLink href='#skills'>Skills</NavLink>
           <NavLink href='#experience'>Experience</NavLink>
           <NavLink href='#projects'>Projects</NavLink>
-          <NavLink href='#education'>Education</NavLink>
+          {/* <NavLink href='#education'>Education</NavLink> */}
         </NavItems>
         <ButtonContainer>
           <GitHubButton href={Bio.github} target="_blank">Github Profile</GitHubButton>
